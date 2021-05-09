99f3b3a685c90f8d0c1bf781e324f6e496057443
JoelCCodes <32679235+JoelCCodes@users.noreply.github.com>
2021-05-09 12:00:18 -0400

    admin

--- pages/admin.vue
+++ pages/admin.vue
@@ -9,6 +9,12 @@
     <h1>Layer 2 fDai Contract</h1>
     <h2>Total Supply of fDai</h2>
     <span>${{ totalSupply }}</span>
+    <br />
+    <h1>Rebalance</h1>
+    <span
+      ><b-button @click="rebalanceL1"></b-button
+      ><b-input v-model="rebalanceSendingAmount"></b-input
+    ></span>
   </div>
 </template>
 
@@ -23,11 +29,26 @@ export default {
       totalDeposit: 0,
       uninvestedFunds: 0,
       totalSupply: 0,
+      Layer1Balance: 0,
+      rebalanceSendingAmount: 0,
     }
   },
   computed: {},
 
-  methods: {},
+  methods: {
+    rebalanceL1() {
+      this.farmtopiainterface.methods
+        .rebalanceL1(
+          this.rebalanceSendingAmount,
+          '0x72Ac29939810d7B1B03aF76D65371eaACaAd7193'
+        )
+        .send({ from: this.loggedInAccount })
+        .then((result) => {
+          console.log(result)
+          return result
+        })
+    },
+  },
   async beforeMount() {},
 
   async mounted() {},
@@ -41,6 +62,11 @@ export default {
       farmtopiaInterface.abi,
       '0xfE6a16D577854b6502d9b32B9683d0f56f3fA863'
     )
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
+    //We take the first address in the array of addresses and display it
+    this.loggedInAccount = accounts[0]
+
     console.log(this.fDaiInstance, this.farmtopiainterface)
     this.totalDeposit =
       (await this.farmtopiainterface.methods.totalDeposit().call()) /
